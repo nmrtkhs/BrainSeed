ebbaf76aca85f03cbccb302a28a5ca9ff4c54b46
nmrtkhs <nmrtkhs@gmail.com>
2021-03-17 14:42:50 +0900

    Skill master 追加

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 
 namespace Seed
@@ -13,23 +14,34 @@ namespace Seed
         protected int _maxhp = 10;
         protected int _attack = 1;
         protected int _defense = 1;
+        protected int _skilId1 = 0;
+        protected int _skilId2 = 0;
+        protected int _skilId3 = 0;
+        protected int _skilId4 = 0;
+        protected SkillMasterAsset _skillMaster;
 
         public int CalcAttack(int skill = 0)
         {
             int attackPower = _attack * Random.Range(0, 3);
-            if (skill == 1)
+            if (skill == 1 && _skilId1 != 0)
             {
-                attackPower += 1;// スキルが0以上の場合はここでattackPowerに対して効果をかける
-                // attackPower *= buff
+                var skillMaster = _skillMaster.SkillMasterList.Where(skillMaster => skillMaster.id == _skilId1).FirstOrDefault();
+                Debug.Log(skillMaster.SkillName);
+                Debug.Log($"before attack:{attackPower}");
+                if (skillMaster != null)
+                {
+                    attackPower += attackPower * skillMaster.ratio / 100;
+                }
+                Debug.Log($"after attack:{attackPower}");
             }
-            if (skill == 2)
+            if (skill == 2 && _skilId2 != 0)
             {
                 attackPower += 2;
             }
-            if (skill == 3)
+            if (skill == 3 && _skilId3 != 0)
             {
                 attackPower += 3;
-            }if (skill == 4)
+            }if (skill == 4 && _skilId4 != 0)
             {
                 attackPower += 4;
             }
@@ -76,8 +88,17 @@ namespace Seed
             _level = PlayerPrefs.GetInt("LV", 1);
             _attack = PlayerPrefs.GetInt("ATTACK", 1);
             _hp = PlayerPrefs.GetInt("HP", 10);
-             _maxhp = PlayerPrefs.GetInt("HP", 10);
+            _maxhp = PlayerPrefs.GetInt("HP", 10);
             _defense = PlayerPrefs.GetInt("DEFENSE", 1);
+            _skilId1 = PlayerPrefs.GetInt("SKILL_ID_1", 0); 
+            _skilId2 = PlayerPrefs.GetInt("SKILL_ID_2", 0); 
+            _skilId3 = PlayerPrefs.GetInt("SKILL_ID_3", 0); 
+            _skilId4 = PlayerPrefs.GetInt("SKILL_ID_4", 0); 
+
+            _skillMaster = Resources.Load("SkillMasterList") as SkillMasterAsset;
+
+            // TODO テスト用
+            _skilId1 = 1;
         }
     }
 
---
+++ Assets/Script/SkillMasterAsset.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/CreateSkillMaster")]
+public class SkillMasterAsset : ScriptableObject
+{
+    public List<SKillMaster> SkillMasterList = new List<SKillMaster>();
+}
+
+[System.Serializable]
+public class SKillMaster
+{
+    public int id = 0;
+    public string SkillName = "がんせきおとし";
+
+    [SerializeField]
+    public int ratio = 100;
+}
